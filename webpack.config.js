4c36542083762ca04d2516e3adbd4589ad186cc7
aleksander-omelchuk <aleksander.omelchuk@gmail.com>
2022-10-19 14:55:47 +0300

    comit

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,22 @@ output:{
     path: path.resolve(__dirname, 'build'),
     filename: 'my-bundle.js',
 },
+module: {
+    rules: [{
+        test: /\.scss$/i,
+        use: ["css-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      }
+    ],
+  },
+devServer: {
+    port: 8888,
+    open: true,
+    stats: 'errors-only',
+  },
 
 };
